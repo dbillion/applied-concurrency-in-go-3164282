20636b0089221767bc8dae3c79ce6403f290a0d1
Oludayo Adeoye <49393855+dbillion@users.noreply.github.com>
2024-05-24 13:33:17 +0000

    channel works

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// create a channel
-	ch := make(chan string)
+	ch := make(chan string, 1)
 	// start the greeter to provide a greeting
 	go greet(ch)
 	// sleep for a long time
@@ -23,7 +23,7 @@ func main() {
 }
 
 // greet writes a greet to the given channel and then says goodbye
-func greet(ch chan string) {
+func greet(ch chan<- string) {
 	fmt.Printf("Greeter ready!\nGreeter waiting to send greeting...\n")
 	// greet
 	ch <- "Hello, world!"
